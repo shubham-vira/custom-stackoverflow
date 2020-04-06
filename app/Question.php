223f7bc1539ec05651ab5fc521297dfc771b6254
shubham-vira <shubhamvira@gmail.com>
2020-04-06 18:54:19 +0530

    controller

---
+++ app/Question.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Support\Str;
+class Question extends BaseModel
+{
+    public function owner(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+    public function setTitleAttribute($title){
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
+    }
+
+    public function getUrlAttribute(){
+        return "questions/{$this->id}";
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getAnswersStylesAttribute(){
+        if($this->answers_count > 0)
+        {
+            if($this->best_answer_id){
+                return "has-best-answer";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+
+}
---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">All Questions</div>
+
+                    @foreach($questions as $question)
+                        <div class="card-body">
+                            <div class="media">
+                                <div class="d-flex flex-column mr-4 statistics">
+                                    <div class="text-center mb-3">
+                                        <strong class="d-block"> {{ $question->votes_count }}</strong>
+                                        Votes
+                                    </div>
+                                    <div class="text-center mb-3 answers {{ $question->answers_styles }}">
+                                        <strong class="d-block"> {{ $question->answers_count }}</strong>
+                                        Answers
+                                    </div>
+                                    <div class="text-center">
+                                        <strong class="d-block"> {{ $question->views_count }}</strong>
+                                        Views
+                                    </div>
+
+                                </div>
+                                <div class="media-body">
+
+                                    <h4><a href="{{ $question->url }}">{{$question->title}}</a></h4>
+                                    <p> Asked By:
+                                        <a href="#">{{ $question->owner->name }}</a>
+                                        <span class="text-muted"> {{ $question->created_date }}</span>
+                                    </p>
+                                    <p>{{ Str::limit($question->body,250) }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                    <br>
+                    <div class="card-footer">
+                        {{ $questions->links() }}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
